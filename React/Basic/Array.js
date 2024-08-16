745728c483a39aa7c5f499845a76b7b890bd7837
Ajmaldevala <ajmaldevala@gmail.com>
2024-08-16 17:44:22 +0530

    basic code added

---
+++ React/Basic/Array.js
@@ -0,0 +1,17 @@
+// basic filter
+const a = [
+    {
+      name: "ajmal"
+    },
+    {
+      name: "hisham"
+    },
+    {
+      name: "ajith"
+    }]
+    
+    // let b = a.filter(a=>a.name!=="ajmal") // filter the rest of the data 
+    // let b = a.filter(a=>a.name =="ajmal") //return the same array but you need just the object use find  
+    // let c = a.find(a=>a.name =="ajmal") //return only the object 
+  
+    console.log(c)
--- React/Basic/script.js
+++ React/Basic/script.js
@@ -1,9 +1,34 @@
-const a = [1, 2, 3, 4, 5, 5, 6, 6, 7, 6, 6, 7];
+const data = [1, 2, 3, 4, 5, 5, 6, 6, 7, 6, 6, 7];
 
-for (let i = 0; i < a.length; i++) {
-  if (a[i] % 2 !== 0) {
-    console.log(a[i],"Odd"); // Print only the odd numbers
-  }else {
-    console.log(a[i])
+// Separate the odd and even numbers
+let odd = [];
+let even = [];
+for (let i = 0; i < data.length; i++) {
+  if (data[i] % 2 !== 0) {
+    odd.push(data[i]);
+  } else {
+    even.push(data[i]);
   }
 }
+// console.log("Odd numbers:", odd);
+// console.log("Even numbers:", even);
+
+// ........//...........................................................................................................
+
+function isValidParentheses(s) {
+  let balance = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(") {
+      balance++;
+    } else if (s[i] === ")") {
+      balance--;
+    }
+    if (balance < 0) {
+      return false;
+    }
+  }
+  return balance === 0;
+}
+// ........//...........................................................................................................
+
+// console.log(isValidParentheses("([])()"));
