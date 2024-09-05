2572ba8b3d925df9efa9fce5965b6415cb16ed6c
Ajmaldevala <ajmaldevala@gmail.com>
2024-09-05 16:36:12 +0530

    basic array

---
+++ javasript/Advanced/Readmore.js
@@ -0,0 +1,28 @@
+// JavaScript has 8 Datatypes
+String;
+Number;
+Boolean;
+Undefined;
+Null;
+Object;
+
+// Numbers:
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const person = { firstName: "John", lastName: "Doe" };
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25");
--- javasript/Basic/Array.js
+++ javasript/Basic/Array.js
@@ -1,3 +1,6 @@
+// The Array object is used to store multiple values in a single variable.
+const cars = ["Saab", "Volvo", "BMW",2,5,5.5,7.1,{name:"ajmal",age:50 } ,true , null ,undefined];  // string ,number,integer ,object , Booleans ,null , undifiend 
+
 // basic filter
 const a = [
   {
@@ -25,10 +28,20 @@ console.log(
   D.filter((a) => ["boolean", "string", "object"].includes(typeof a))
 );
 
+
+
 const totalSum = a.reduce((sum, invoice) => sum + parseInt(invoice.Amount), 0); //  in the case where the array contains numbers as strings, use the parseInt() method to convert them to numbers
 console.log(totalSum);
+
 
 const aa = [1, 2, 5];
 const bb = [1, 4, 6, 5];
 let c = aa.filter((a) => bb.includes(a));
 console.log(c);
+
+
+
+let test = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for(let i=0; i <test.length; i++){
+    console.log(test[i] % 4, test[i] % 4 < 2) //1 2 3 0 1 2 3 0 1 2 3 0  1 false true true false false true true 
+}
