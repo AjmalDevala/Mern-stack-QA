8c8e1652cb3fac343ade2902c664a05eb50e1971
Ajmaldevala <ajmaldevala@gmail.com>
2024-11-16 17:24:54 +0530

    Docs: Basic Array Questions

--- javasript/Basic/Array.js
+++ javasript/Basic/Array.js
@@ -19,11 +19,11 @@ const a = [
   },
 ];
 
-// let b = a.filter(a=>a.name!=="ajmal") // filter the rest of the data
+let b = a.filter(a=>a.name!=="ajmal") // filter the rest of the dataṇ
 // let b = a.filter(a=>a.name =="ajmal") //return the same array but you need just the object use find
 // let c = a.find(a=>a.name =="ajmal") //return only the object
 
-// console.log(c)
+console.log(a)
 
 const D = [1, 2, 3, 4, true, false, "ajmal", { name: "ajmal" }]; // filter the type
 // console.log(
